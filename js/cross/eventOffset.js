816e7b122e7458ea17719e1c622d8dba2d3b9672
Ivanko <checkan_gold@ukr.net>
2015-08-30 13:05:19 +0300

    Fixed bug with hoisting into polyfill

--- js/cross/eventOffset.js
+++ js/cross/eventOffset.js
@@ -1,6 +1,7 @@
 var eventOffset = function(evt, type){
+	var targetType;
 	type = type.toUpperCase();
-	var targetType = (type === 'X') ? 'Left' : 'Top';
+	targetType = (type === 'X') ? 'Left' : 'Top';
 	
 	if(typeof evt['offset'+type] == 'undefined'){
 		evt.offset['offset'+type] = evt['layer'+type] - evt.currentTarget['offset'+targetType];
